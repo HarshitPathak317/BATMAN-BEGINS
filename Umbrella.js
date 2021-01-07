d8a1c6b38eb817bcd4b7ebbb068b5a8a3ed0931b
HarshitPathak317 <70531158+HarshitPathak317@users.noreply.github.com>
2021-01-07 19:04:35 +0530

    Add files via upload

--- Umbrella.js
+++ Umbrella.js
@@ -1,19 +1,17 @@
 class Umbrella {
-    constructor(x,y,width,height) {
+  constructor(x,y){
       var options = {
-          isStatic: true
+          isStatic: true,
       }
-      this.width = width;
-      this.height = height;
-      this.body = Bodies.circle(x,y,50,options);
-      this.image = loadImage("images/Walking Frame/walking_1.png","images/Walking Frame/walking_2.png","images/Walking Frame/walking_3.png","images/Walking Frame/walking_4.png","images/Walking Frame/walking_5.png","images/Walking Frame/walking_6.png","images/Walking Frame/walking_7.png","images/Walking Frame/walking_8.png")
-      this.image.scale = 0.7;
-      World.add(world, this.body);
-    }
-    display(){
-      //var pos =this.body.position;
+      this.image = loadImage("images/Walking Frame/walking_1.png");
+      this.umbrella = Bodies.circle(x,y,50,options);
+      this.radius = 50;
+      World.add(world, this.umbrella)
+  }
+
+  display(){
+      var pos = this.umbrella.position;
       imageMode(CENTER);
-      image(this.image,200,400,500,600)
-      //rect(pos.x, pos.y, this.width, this.height);
-    }
-  };
+      image(this.image,pos.x,pos.y+70,300,300);
+  }
+}
